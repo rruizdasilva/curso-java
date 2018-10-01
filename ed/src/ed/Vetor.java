8012a73aaed419322c855587a2af4e3a5748c146
Roger Ruiz da Silva <rruizdasilva@yahoo.com.br>
2018-09-30 22:19:08 -0300

    Coding...

---
+++ ed/src/ed/Vetor.java
@@ -0,0 +1,34 @@
+package ed;
+
+import java.util.Arrays;
+
+public class Vetor {
+	
+	private Aluno[] alunos = new Aluno[100];
+	
+	public void adiciona(Aluno aluno) {
+		
+	}
+	
+	public Aluno pega(int posicao) {
+		return null;
+	}
+	
+	public void remove(int posicao) {
+		
+	}
+	
+	public boolean contem(Aluno aluno) {
+		return false;
+	}
+	
+	public int tamanho() {
+		return 0;
+	}
+	
+	@Override
+	public String toString() {
+		return Arrays.toString(alunos);
+	}
+
+}
